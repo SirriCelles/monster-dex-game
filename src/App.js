7e4b28be6f3bce4fb94e463c0641386d3f9281bd
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-11-02 10:29:59 +0100

    monster game intro

--- src/App.js
+++ src/App.js
@@ -7,35 +7,36 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {
-        firstName: 'Sirri',
-        lastName: 'Celles'
-      },
-      company: 'herSkills'
+      monsters: [
+        {
+          name: 'Linda',
+          id: '12rfdjldl93u'
+        },
+        {
+          name: 'Frank',
+          id: '12rfdjldlo3u'
+        }, 
+        {
+          name: 'Jacky',
+          id: '12rkdjldl93u'
+        },
+        {
+          name: 'Jed',
+          id: '124fdjldl93u'
+        }
+      ]
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button
-            onClick={(state, props) => {this.setState(
-              () => {
-                return {
-                  name: {firstName: 'John', lastName: 'Wayne'}
-                }
-              }, () => {
-                
-              }
-            );}}
-          >
-            Learn React
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>;         
+          })
+        }
       </div>
     );
   }
