a46a65cbecc0d9b5ca6e6e21ad2c3293d628af3c
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-11-09 20:24:41 +0100

    adding new components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
 
     // init state
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -25,22 +27,34 @@ class App extends Component {
     ));
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(
+      () => {
+        return {searchField}
+      }
+    );
+  }
 
   // this runs next and termiines what to show
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter(el => el.name.toLocaleLowerCase().includes(searchField));
+
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monters' 
-          onChange={(event) => {
-            
-          }} />
+          onChange={onSearchChange} />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>;         
           })
         }
+
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+// When you return a component, make sure there is only one top level component return that is 
+// one top level top HTML tag.
+
+class CardList extends Component {
+    render() {
+        const { monsters } = this.props;
+        return (
+            <div>
+                {
+                    monsters.map((monster) => {
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default CardList;
